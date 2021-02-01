dac696acc480a20ff40d864704f3b0f0fb64ac07
Suraj <hrishabhsuraj52@gmail.com>
2021-02-01 16:04:59 +0530

    New commit

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -1,6 +1,25 @@
-#Helper functions
-#***********************************************************************
-def print_qty(water,milk,coffee, disposable_cups,money):
+#Defining the coffee machine class
+class CoffeeMachine:
+    def __init__(self,water,milk,coffee,disposable_cups,money):
+        self.water = water
+        self.milk = milk
+        self.coffee = coffee
+        self.disposable_cups = disposable_cups
+        self.money = money
+    #Method for printing acknowledgement of processing/!processing coffee
+    def acknowledgment(self,flag, *args):
+        self.flag = flag
+        if self.flag=="ok":
+            print("I have enough resources, making you a coffee!")
+            print()
+        elif self.flag=="error":
+            finished_ingredient = args[0]
+            print("Sorry, not enough {0}!".format(finished_ingredient))
+            print()
+
+#function for displaying contents
+#displaying method
+def print_qty(water,milk,coffee,disposable_cups,money):
     print("The coffee machine has:")
     print("{0} of water".format(water))
     print("{0} of milk".format(milk))
@@ -11,15 +30,8 @@ def print_qty(water,milk,coffee, disposable_cups,money):
     else:
         print("{0} of money".format(money))
     print()
-#acknowledgement of having coffee
-def acknowledgment(flag, *args):
-    if flag=="ok":
-        print("I have enough resources, making you a coffee!")
-        print()
-    elif flag=="error":
-        finished_ingredient = args[0]
-        print("Sorry, not enough {0}!".format(finished_ingredient))
-        print()
+
+#**********************************************************************
 # Init amount
 water = 400
 milk = 540
@@ -28,6 +40,8 @@ disposable_cups = 9
 money = 550
 #**************************************************************************
 #Actions begins
+#Instantiate the class
+CoffeeMaker = CoffeeMachine(water,milk,coffee,disposable_cups,money)
 while(True):
     input_action = input("Write action (buy, fill, take, remaining, exit):")
     print()
@@ -41,7 +55,7 @@ while(True):
         buy_choice = (input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:"))
         if buy_choice =="1":
             if water-250>0 and coffee-16>0:
-                acknowledgment(flag="ok")
+                CoffeeMaker.acknowledgment(flag ="ok")
                 disposable_cups = disposable_cups-1
                 water,coffee,money =  (water-250, coffee-16, money+4)
             else:
@@ -53,11 +67,11 @@ while(True):
                     finished_ingredient ="water"
                 else:
                     finished_ingredient = "coffee"
-                acknowledgment("error", finished_ingredient)
+                CoffeeMaker.acknowledgment("error", finished_ingredient)
 
         elif buy_choice =="2":
             if water-350>0 and milk-75>0 and coffee-20>0 :
-                acknowledgment(flag="ok")
+                CoffeeMaker.acknowledgment(flag="ok")
                 water,milk, coffee,money = water-350,milk-75, coffee-20, money+7
                 disposable_cups = disposable_cups-1
             else:
@@ -72,12 +86,12 @@ while(True):
                     finished_ingredient = "milk"
                 else:
                     finished_ingredient="coffee"
-                acknowledgment("error", finished_ingredient)
+                CoffeeMaker.acknowledgment("error", finished_ingredient)
             #water,milk,coffee,money = latte(water,milk,coffee,money)
             #disposable_cups = disposable_cups-1
         elif buy_choice=="3":
             if water-200>0 and milk-100>0 and coffee-12>0 :
-                acknowledgment(flag="ok")
+                CoffeeMaker.acknowledgment(flag="ok")
                 water,milk,coffee,money = (water-200,milk-100,coffee-12, money+6)
                 disposable_cups = disposable_cups-1
             else:
@@ -92,7 +106,7 @@ while(True):
                     finished_ingredient = "milk"
                 else:
                     finished_ingredient="coffee"
-                acknowledgment("error", finished_ingredient)
+                CoffeeMaker.acknowledgment("error", finished_ingredient)
     elif input_action=="fill":
         water+= int(input("Write how many ml of water do you want to add:"))
         milk+= int(input("Write how many ml of milk do you want to add:"))
